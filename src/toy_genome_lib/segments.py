9c3bc50762c5ce58f5b3caa36afe7940c79e67b1
Alexey Nesterenko <comcon1@protonmail.com>
2025-09-26 16:10:21 +0200

    test algo with naive code

--- src/toy_genome_lib/segments.py
+++ src/toy_genome_lib/segments.py
@@ -1,6 +1,9 @@
-"""_summary_
 """
-import math
+Segments module.
+
+Defines _Segments_ class for working with the list of segments and
+_SegmentFileFormatError_ exception class.
+"""
 import os
 
 
@@ -17,14 +20,29 @@ class Segments:
     def data(self):
         return self._data
 
+    def coverage(self):
+        sum = 0
+        for b, e in self._data.items():
+            sum += (e-b)
+        return sum
+
+    def __len__(self):
+        return len(self._data)
+
     def __init__(self, ar: dict):
         self._data = ar
-    
+
+    def to_file(self, file_path: str):
+        with open(file_path, 'w') as f:
+            for i, j in self._data.items():
+                f.write(f"{i} {j}\n")
+
     @classmethod
     def from_file(cls, file_path: str):
         """Reads segments from a file.
 
-        Each line in the file should contain two integers representing the start and end of a segment.
+        Each line in the file should contain two integers representing
+        the start and end of a segment.
 
         Args:
             file_path (str): Path to the segments file.
@@ -41,7 +59,7 @@ class Segments:
         dic = {}
         last_end = -1
         with open(file_path, 'r') as f:
-            for i,line in enumerate(f):
+            for i, line in enumerate(f):
                 parts = line.strip().split()
                 if len(parts) != 2:
                     msg = f"Invalid line format at line {i+1}: {line.strip()}"
@@ -51,11 +69,14 @@ class Segments:
                 except ValueError:
                     raise SegmentFileFormatError from ValueError
                 if start < last_end or start >= end:
-                    msg = f"Segments must be non-overlapping and start < end. Error at line {i+1}: {line.strip()}"
+                    msg = (
+                            "Segments must be non-overlapping and start < end."
+                            f" Error at line {i+1}: {line.strip()}"
+                    )
                     raise SegmentFileFormatError(msg)
                 dic[start] = end
         return cls(dic)
-    
+
     def overlap(self, other):
         """Calculates the total overlap length with another Segments instance.
 
@@ -65,29 +86,28 @@ class Segments:
         Returns:
             int: Total length of overlapping segments.
         """
-        total_overlap = 0
-        start_others = list(other.data.keys())
+        overlap_dict = {}
 
         other_it = iter(other.data.items())
-        start_others, end_others = next(other_it) # Initialize first segment
+        start_others, end_others = next(other_it)  # Initialize first segment
 
         for start_self, end_self in self.data.items():
             while start_others < end_self:
                 if start_others >= start_self:
                     overlap_start = start_others
                     overlap_end = min(end_self, end_others)
-                    total_overlap += overlap_end - overlap_start
-                else: # start_others < start_self
+                    overlap_dict[overlap_start] = overlap_end
+                else:  # start_others < start_self
                     if end_others > start_self:
                         overlap_start = start_self
                         overlap_end = min(end_self, end_others)
-                        total_overlap += overlap_end - overlap_start
+                        overlap_dict[overlap_start] = overlap_end
                 try:
                     start_others, end_others = next(other_it)
                 except StopIteration:
                     break
 
-        return total_overlap
+        return Segments(overlap_dict)
 
 #--------------------------------------------------------------------------------
 #12345678901234567890123456789012345678901234567890123456789012345678901234567890
--- tests/test_segments.py
+++ tests/test_segments.py
@@ -1,15 +1,83 @@
 import pytest
+import random
 from toy_genome_lib.segments import Segments, SegmentFileFormatError
 import os
 
-@pytest.mark.unit
-def test_read_segments():
+RANDLEN = 1000
+
+
+@pytest.fixture
+def seg1():
     seg_file = os.path.join(os.path.dirname(__file__), "data", "seg1.s")
     segs = Segments.from_file(seg_file)
-    assert len(segs.data) == 5
-    assert segs.data[100] == 200
-    assert segs.data[1035] == 2412
+    yield segs
+
+
+@pytest.fixture
+def seg_rand_factory():
+    def _make():
+        b = 0
+        dic = {}
+        while True:
+            step = random.randint(0, 50)
+            a = b + step
+            n = random.randint(1, 50)
+            b = a + n
+            if a >= RANDLEN-1 or b > RANDLEN:
+                break
+            else:
+                dic[a] = b
+        return Segments(dic)
+    return _make
+
+
+@pytest.mark.unit
+def test_read_segments(seg1):
+    assert len(seg1.data) == 5
+    assert seg1.data[100] == 200
+    assert seg1.data[1035] == 2412
 
     # Test with non-existing file
     with pytest.raises(FileNotFoundError):
         Segments.from_file("non_existing_file.s")
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize("_", range(20))  # repeat for 20 random segments
+def test_segments_coverage(seg_rand_factory, _):
+    seg_rand_1 = seg_rand_factory()
+    cov = seg_rand_1.coverage()
+    # naive coverage
+    a = [0] * RANDLEN
+    li = []
+    for i, j in seg_rand_1.data.items():
+        li += a[i:j]
+    if len(li) != cov:
+        seg_rand_1.to_file('bad.s')
+        assert False
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize("_", range(20))
+def test_segments_intersection(seg_rand_factory, _):
+    seg_rand_1 = seg_rand_factory()
+    seg_rand_2 = seg_rand_factory()
+    oseg = seg_rand_1.overlap(seg_rand_2)
+    rseg = seg_rand_2.overlap(seg_rand_1)
+    ans1 = oseg.coverage()
+    # naive overlap
+    a = [0] * RANDLEN
+    for i, j in seg_rand_1.data.items():
+        a[i:j] = [1]*(j-i)
+    b = [0] * RANDLEN
+    for i, j in seg_rand_2.data.items():
+        b[i:j] = [1]*(j-i)
+    c = [ a[i]*b[i] for i in range(RANDLEN) ]
+    ans2 = sum(c)
+    if ans2 != ans1:
+        seg_rand_1.to_file('bad1.s')
+        seg_rand_2.to_file('bad2.s')
+        oseg.to_file('bad_12.s')
+        rseg.to_file('bad_21.s')
+        pytest.fail(f'Not the same {ans2} / {ans1}')
+
